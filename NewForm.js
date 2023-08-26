768518d31044158ad6188d69b2e2bf67cb5fae56
Aishu-1108 <aishwaryaanand1108@gmail.com>
2023-08-26 12:03:08 +0530

    form with fields, nav-bar, drop-down and signature

---
+++ NewForm.js
@@ -0,0 +1,8 @@
+function clickHere(){
+    console.log("Welcome to JavaScript");
+    alert('Continue for home page');
+}
+function doubleClick(){
+    console.log("Double click to find the alert");
+    alert('Double click on the contact');
+}
